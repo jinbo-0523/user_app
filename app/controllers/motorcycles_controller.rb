2b6135f417e37ec67a5a65babb0d7b90669e0c7f
jinbo-0523 <mi2ma3.0523@gmail.com>
2020-09-08 11:49:40 +0900

    バリデーションエラー時に投稿ページをレンダリングする様に修正

--- app/controllers/motorcycles_controller.rb
+++ app/controllers/motorcycles_controller.rb
@@ -1,5 +1,6 @@
 class MotorcyclesController < ApplicationController
   before_action :set_motorcycle, only: %i[show edit update destroy]
+  
   def index
     @motorcycles = Motorcycle.order(id: :asc)
   end
@@ -9,8 +10,13 @@ class MotorcyclesController < ApplicationController
   end
 
   def create
-    motorcycle = Motorcycle.create!(motorcycle_params)
-    redirect_to motorcycle,notice:"投稿しました"
+    @motorcycle = Motorcycle.new(motorcycle_params)
+    if @motorcycle.save
+      redirect_to @motorcycle,notice:"投稿しました"
+    else
+      flash.now[:alert] = "投稿に失敗しました"
+      render :new
+    end
   end
 
   def show
@@ -20,15 +26,18 @@ class MotorcyclesController < ApplicationController
   end
 
   def update
-    @motorcycle.update(motorcycle_params)
+    if @motorcycle.update(motorcycle_params)
     redirect_to root_path, notice: "更新しました"
+    else
+      flash.now[:alert] = "投稿に失敗しました"
+      render :edit
+    end
   end
 
   def destroy
     @motorcycle.destroy!
     redirect_to root_path, alert: "削除しました"
   end
-
 
   def set_motorcycle
     @motorcycle = Motorcycle.find(params[:id])
