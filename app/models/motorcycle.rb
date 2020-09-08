ca6022d4a9d098eb88e50eefa71d5f6243677da4
jinbo-0523 <mi2ma3.0523@gmail.com>
2020-09-08 10:53:01 +0900

    car_typeをCAR_TYPE_LISTとして定義

--- app/models/motorcycle.rb
+++ app/models/motorcycle.rb
@@ -1,5 +1,6 @@
 class Motorcycle < ApplicationRecord
+  CAR_TYPE_LIST = %w(ネイキッド アメリカン スーパースポーツ オフロード アドベンチャー ツアラー ビックスクーター 原付).freeze
   validates :name, presence: true, length: { maximum: 30 } 
   validates :displacement, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 50, less_than_or_equal_to: 2500 }
-  validates :car_type, presence: true, inclusion:{ in: %w(ネイキッド アメリカン スーパースポーツ オフロード アドベンチャー ツアラー ビックスクーター 原付)} 
+  validates :car_type, presence: true, inclusion:{ in: CAR_TYPE_LIST } 
 end
